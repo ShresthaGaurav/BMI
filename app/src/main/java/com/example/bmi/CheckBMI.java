049a9923be28e3f7720129a1523d1e03aa9eda96
ShresthaGaurav <gauravshrestha202@gmail.com>
2019-10-17 21:19:39 +0545

    Initial commit

---
+++ app/src/main/java/com/example/bmi/CheckBMI.java
@@ -0,0 +1,21 @@
+package com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CheckBMI extends AppCompatActivity {
+TextView txtresult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_bmi);
+        txtresult=findViewById(R.id.textresult);
+        Intent intent = getIntent();
+        double bmi= Double.parseDouble(intent.getStringExtra("bmi"));
+new Calculate().CheckBmi(txtresult,bmi);
+    }
+
+}
